1bbac42dc6d44a16f80b985b97189d3774da1e6c
M8IN <M8IN@LAPTOP-820ERDM2.mshome.net>
2025-12-06 15:32:11 +0530

    local arraymax

--- RestAL/src/test/java/array/Array_Max.java
+++ RestAL/src/test/java/array/Array_Max.java
@@ -5,11 +5,9 @@ public class Array_Max {
 	public static void main(String[] args) { 
 		int a[]= {2,1,9,1}; 
 		int max=a[0];
-		  System.out.println("local4 branch");
 		for (int i=1;i<a.length;i++){
 			if(a[i]>max) {
 				max=a[i];}
-			 System.out.println("local4 branch");
 			}
 		System.out.println("local ==="+max); 
 		//==========================================================
