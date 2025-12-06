5c21d516312c934edc9f52e714fe397c3a67d10c
M8IN <M8IN@LAPTOP-820ERDM2.mshome.net>
2025-12-06 14:17:00 +0530

    3 class change

--- RestAL/src/test/java/array/Array_Max.java
+++ RestAL/src/test/java/array/Array_Max.java
@@ -5,9 +5,11 @@ public class Array_Max {
 	public static void main(String[] args) { 
 		int a[]= {2,1,9,1}; 
 		int max=a[0];
+		  System.out.println("local2 branch");
 		for (int i=1;i<a.length;i++){
 			if(a[i]>max) {
 				max=a[i];}
+			  System.out.println("local2 branch");
 			}
 		System.out.println("maximum number ==="+max); 
 		//==========================================================
@@ -16,6 +18,7 @@ public class Array_Max {
 		{
 			if(a[i]<min) {
 			min=a[i];
+			  System.out.println("local2 branch");
 			}
 		}
 		System.out.println("minimum number ==="+min);
--- RestAL/src/test/java/array/Count_No_Array.java
+++ RestAL/src/test/java/array/Count_No_Array.java
@@ -3,7 +3,7 @@ package array;
 import java.util.HashMap;
 import java.util.Map;
 
-public class count_No_Array {
+public class Count_No_Array {
 	 public static void main(String[] args) {
 	        // Initialize the array
 	        int b[] = {2, 7, 1, 2, 7};
@@ -13,10 +13,12 @@ public class count_No_Array {
 
 	        // Count occurrences of each number in the array
 	        for (int i = 0; i < b.length; i++) {
+	        	  System.out.println("local2 branch");
 	            int c = b[i];
 	            if (map.containsKey(c)) {
 	                int cnt = map.get(c);
 	                map.put(c, ++cnt);
+	                System.out.println("local2 branch");
 	            } else {
 	                map.put(c, 1);
 	            }
@@ -34,9 +36,11 @@ public class count_No_Array {
 	        // Count and print occurrences for each number from 0 to 99
 	        for (int i = 0; i < 100; i++) {
 	            int cnt = 0;
+	            System.out.println("local2 branch");
 	            for (int j = 0; j < b.length; j++) {
 	                if (i == b[j]) {
 	                    cnt++;
+	                    System.out.println("local2 branch");
 	                }
 	            }
 	            if (cnt != 0) {
