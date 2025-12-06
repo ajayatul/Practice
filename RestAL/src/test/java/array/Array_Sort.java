9ecfa2fa84078f351f1c72ac0fdd8491f7d0dd0f
M8IN <M8IN@LAPTOP-820ERDM2.mshome.net>
2025-12-06 12:51:08 +0530

    local commit

--- RestAL/src/test/java/array/Array_Sort.java
+++ RestAL/src/test/java/array/Array_Sort.java
@@ -3,7 +3,7 @@ package array;
 public class Array_Sort {
 	public static void main(String[] args) { 
 		int a[]= {12,4,1,9,7}; 
-		int max=a[0]; 
+	//	int max=a[0]; 
 		for(int i=0;i<a.length;i++) {
 			for(int j=i+1;j<a.length;j++)
 				if(a[i]<a[j]) 
--- RestAL/src/test/java/array/count_No_Array.java
+++ RestAL/src/test/java/array/count_No_Array.java
@@ -24,7 +24,7 @@ public class count_No_Array {
 
 	        // Print the frequency map
 	        System.out.println(map);
-	       System.out.println("abc");
+	       System.out.println("local code ");
 		    System.out.println("===============================");
 
 	        // Count and print occurrences for each number from 0 to 99
